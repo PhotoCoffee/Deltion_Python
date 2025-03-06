d3c3b82bb52f8cf0d75d3c0fadd731a2b9cf8864
PhotoCoffee <b.j.d.haak@live.nl>
2025-03-06 18:36:16 +0100

    Wat is jouw favoriete nederlandse zanger?

--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -2,8 +2,12 @@
 # Naam student:
 # Groep:
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+dict_0 = { "id": 0, "voornaam": "Willeke", "achternaam": "Alberti" }
+dict_1 = { "id": 1, "voornaam": "Alexander", "achternaam": "Curly" }
+dict_2 = { "id": 2, "voornaam": "Cornelis", "achternaam": "Vreeswijk" }
+dict_3 = { "id": 3, "voornaam": "Gerard", "achternaam": "Schoonebeek" }
+lijst = [dict_0, dict_1, dict_2, dict_3]
+
+
+nameprintid = 1
+print(lijst[nameprintid]["voornaam"], lijst[nameprintid]["achternaam"])
