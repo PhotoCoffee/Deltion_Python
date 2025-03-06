295129e3ecf132fedbe0cc6408049c1d72d01c10
PhotoCoffee <b.j.d.haak@live.nl>
2025-03-06 21:31:26 +0100

    eerlijk zullen wij alles delen

--- opdrachten/070_condities/opdr_2/opdr_2.py
+++ opdrachten/070_condities/opdr_2/opdr_2.py
@@ -7,7 +7,7 @@
 # Hier start de for-loop
 
 my_list = [43948, 878768, 38768, 87555, 765765]
-
-# for...:
-#   if...:
-#       print()
+print('De getallen die deelbaar zijn door 3 zijn: ')
+for val in my_list:
+    if val % 3 == 0:
+        print(val) # Print de waarde als het deelbaar is door 3
