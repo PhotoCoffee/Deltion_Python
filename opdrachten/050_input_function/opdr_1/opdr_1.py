dbeddab4d65a29dda33039b84921a9db0faab40c
PhotoCoffee <b.j.d.haak@live.nl>
2025-03-06 20:13:45 +0100

    Shoutout Piet Hagelslag

--- opdrachten/050_input_function/opdr_1/opdr_1.py
+++ opdrachten/050_input_function/opdr_1/opdr_1.py
@@ -4,4 +4,11 @@
 
 # Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
 
-
+try:
+    a = int(input('Geef de lengte van de eerste zijde: '))
+    b = int(input('Geef de lengte van de tweede zijde: '))
+except ValueError:
+    print('Dat is geen getal!')
+    exit()
+c = a**2 + b**2
+print(f'De schuine zijde is {c**0.5}')
