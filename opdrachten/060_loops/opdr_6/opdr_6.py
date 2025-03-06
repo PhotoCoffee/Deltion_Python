ba9cf087e6aaf4fde5e3d7641cc2d817f7c9f64d
PhotoCoffee <b.j.d.haak@live.nl>
2025-03-06 21:17:50 +0100

    nu heb ik honger

--- opdrachten/060_loops/opdr_6/opdr_6.py
+++ opdrachten/060_loops/opdr_6/opdr_6.py
@@ -6,4 +6,19 @@
 
 # Hier start de for-loop
 
-my_list = []
+pizzalijst = ['margharita', 'calzone', 'verdi', 'olivio', 'quattro stagioni']
+print(pizzalijst)
+pizzalijst.sort()
+print("op alfabetische volgorde:")
+print(pizzalijst)
+print("ik heb zelf graag hawaii")
+pizzalijst.append('hawaii')
+print(pizzalijst)
+print("ik heb geen zin in verdi")
+pizzalijst.remove('verdi')
+print("de eerste drie pizza's zijn:")
+print(pizzalijst[:3])
+print("de middelste pizza is: ")
+print(pizzalijst[2])
+print("de laatste drie pizza's zijn:")
+print(pizzalijst[-3:])
