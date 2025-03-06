96a61ff4b7c5303bce9f72ae759c25c62559db3b
PhotoCoffee <b.j.d.haak@live.nl>
2025-03-06 20:26:25 +0100

    Uit eten? In deze economie?!?!

--- opdrachten/050_input_function/opdr_2/opdr_2.py
+++ opdrachten/050_input_function/opdr_2/opdr_2.py
@@ -3,5 +3,19 @@
 # Groep:
 
 # Hier komt je code...
+def gastenlijst():
+    print("Restaurantgasten")
+    for i in gasten:
+        print (i)
+    print("\n")
 
-gasten = ["Jij", ... ]
+gasten = ["Ik", "Paul", "Kees", "Marie", "Hilda"]
+gastenlijst()
+
+print("MARIE belt af!")
+gasten.remove("Marie")
+gastenlijst()
+
+print("George wil mee, maar hij wil naast Kees zitten")
+gasten.insert(3, "George")
+gastenlijst()
