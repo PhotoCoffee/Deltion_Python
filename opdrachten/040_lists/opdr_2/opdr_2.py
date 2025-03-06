70cc15113a2386eb793e4fde75f3b280cf896022
PhotoCoffee <b.j.d.haak@live.nl>
2025-03-06 19:55:05 +0100

    nattigheid

--- opdrachten/040_lists/opdr_2/opdr_2.py
+++ opdrachten/040_lists/opdr_2/opdr_2.py
@@ -13,3 +13,21 @@ rivieren = list(rivier_info.keys())
 # rivieren is nu een list met alleen de riviernamen: ['rijn', 'maas', 'nijl']
 
 # Hier jouw code.....
+# Print de naam van de 1e rivier  
+# Print het 2e land waar de 1e rivier doorheen stroomt
+# Zowel land als rivier beginnen met een hoofdletter, gebruik hiervoor een tekstfunctie. 
+
+print(f"De {rivieren[0].capitalize()} stroomt onder andere door {rivier_info[rivieren[0]][1].capitalize()}")
+
+
+#Print de naam van de 2e rivier  
+#Print het 1e land waar de 1e rivier doorheen stroomt
+#Zowel land als rivier beginnen met een hoofdletter 
+
+print(f"De {rivieren[1].capitalize()} stroomt onder andere door {rivier_info[rivieren[1]][0].capitalize()}")
+
+
+#Print de naam van de 3e rivier  
+#Print het 3e land waar de 3e rivier doorheen stroomt
+#Zowel land als rivier beginnen met een hoofdletter 
+print(f"De {rivieren[2].capitalize()} stroomt onder andere door {rivier_info[rivieren[2]][2].capitalize()}")
