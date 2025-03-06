9d1c43780297e7893fad5cef226a78af94c0cbcc
PhotoCoffee <b.j.d.haak@live.nl>
2025-03-06 20:39:24 +0100

    vroem vroem

--- opdrachten/050_input_function/opdr_3/opdr_3.py
+++ opdrachten/050_input_function/opdr_3/opdr_3.py
@@ -4,3 +4,14 @@
 
 # Hier komt je code...
 
+tel = 1
+automerken = []
+while tel < 11:
+    automerken.append(input(f'Noem een automerk {tel}/10: '))
+    tel += 1
+
+automerken.sort()
+automerken.reverse()
+
+print("In omgekeerd alfabetische volgorde is dat:")
+print(automerken)
