02d30d55f0b8c70585154f2bbdc9c99f99a74113
PhotoCoffee <b.j.d.haak@live.nl>
2025-03-07 15:56:32 +0100

    Deze had ik al in de les gemaakt.

--- opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
+++ opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
@@ -3,8 +3,37 @@
 # Groep:
 
 import random
-prompt = "Raad mijn geheime getal \n"
-geheim_getal = random.randint(1, 100)
+getal = random.randint(1, 99)
+gok = 0
+aantal_pogingen = 0
 
-# De rest moet jij doen.....
+while gok != getal:
+    aantal_pogingen += 1
 
+#Sluit het programma als er iets anders dan een geldig getal wordt ingevoerd
+    try: 
+        gok = int(input('Raad het getal tussen 1 en 100: '))
+    except ValueError:
+        print('Dat is geen geldig getal.')
+        exit()
+#Einde van de try-except
+
+#Sluit het programma als het getal buiten de range van 1 tot 100 ligt
+    if gok <= 0:
+        print('Het getal moet groter zijn dan 0.')
+        exit()
+    if gok >= 100:
+        print('Het getal moet lager zijn dan 100.')
+        exit()
+
+#Einde
+
+#Vergelijk gok met getal
+    if gok < getal:
+        print('Het getal is hoger dan ' + str(gok) +'. Probeer het opnieuw!')
+        
+    elif gok > getal:
+        print('Het getal is lager dan ' + str(gok)+'. Probeer het opnieuw!')
+
+
+print('Correct! Het getal was ' + str(getal) + '. Je hebt ' + str(aantal_pogingen) + ' keer gegokt.')
