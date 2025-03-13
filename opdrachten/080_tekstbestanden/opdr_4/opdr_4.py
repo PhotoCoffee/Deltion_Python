1c5f203991e3bb5ec0fa7996cf2eb3358c4531c1
PhotoCoffee <b.j.d.haak@live.nl>
2025-03-13 08:46:11 +0100

    het is feest

--- opdrachten/080_tekstbestanden/opdr_4/opdr_4.py
+++ opdrachten/080_tekstbestanden/opdr_4/opdr_4.py
@@ -4,3 +4,13 @@
 
 
 # Party enquete
+bestand = open("feest.txt", "a")
+bestand.write("----  \n")
+vragenlijst = ['1. Wat is je voornaam?', '2. Wat is je achternaam?', '3. Wat neem je mee aan drank?', '4. Wat neem je mee om te eten?']
+for i in range(4):
+    bestand.write(input(vragenlijst[i] + ' '))
+    bestand.write("\n")
+
+
+print('Bedankt voor het invullen!') 
+print('See you at the party.') 
